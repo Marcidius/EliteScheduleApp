c51a2aecff7a79e55026d8cdb634adf382546694
Ryan King <marcidius@hotmail.com>
2016-05-27 23:05:02 -0400

    Setup standings view with all of the game related data for Wins/Losses/etc... and use the Ionic grid system to do it.

--- www/app/teams/TeamDetailController.js
+++ www/app/teams/TeamDetailController.js
@@ -7,10 +7,57 @@
   angular.module('eliteApp')
     .controller('TeamDetailController', TeamDetailController);
 
-  TeamDetailController.$inject = ['$stateParams'];
-  function TeamDetailController($stateParams) {
+  TeamDetailController.$inject = ['$stateParams', 'eliteApi'];
+  function TeamDetailController($stateParams, eliteApi) {
     var vm = this;
-    console.log('$stateParams', $stateParams);
+    //console.log('$stateParams', $stateParams);
 
-  }
+    vm.teamId = Number($stateParams.id);
+    var data = eliteApi.getLeagueData();
+
+    var team = _.chain(data.teams)
+                .flatten("divisionTeams")
+                .find({"id": vm.teamId })
+                .value();
+
+    vm.teamName = team.name;
+
+    vm.games = _.chain(data.games)
+                  .filter(isTeamInGame)
+                  .map(function (item) {
+                    var isTeam1 = (item.team1Id === vm.teamId ? true : false);
+                    var opponentName = isTeam1 ? item.team2 : item.team1;
+                    var scoreDisplay = getScoreDisplay(isTeam1, item.team1Score, item.team2Score);
+                    return {
+                      gameId: item.id,
+                      opponent: opponentName,
+                      time: item.time,
+                      location: item.location,
+                      locationUrl: item.locationUrl,
+                      scoreDisplay: scoreDisplay,
+                      homeAway: (isTeam1 ? "vs." : "at")
+                    };
+                  })
+                  .value();
+
+
+
+    function isTeamInGame(item) {
+      return item.team1Id === vm.teamId || item.team2Id === vm.teamId;
+    }
+
+    function getScoreDisplay(isTeam1, team1Score, team2Score) {
+      if (team1Score && team2Score) {
+        var teamScore = (isTeam1 ? team1Score : team2Score);
+        var opponentScore = (isTeam1 ? team2Score : team1Score);
+        var winIndicator = teamScore > opponentScore ? "W: " : "L: ";
+
+        return winIndicator + teamScore + "-" + opponentScore;
+      }
+      else {
+        return "";
+      }
+    }
+
+  };
 })();
